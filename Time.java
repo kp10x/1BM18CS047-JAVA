f5f64376c31c58d19e2e7ca3ec92d55216b40a20
kp10x <47416448+kp10x@users.noreply.github.com>
2019-08-23 15:19:44 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,98 @@
+import java.util.*;
+class Timecalc
+{
+ int h,m,s;
+ Timecalc()
+ {
+  h=0;
+  m=0;
+  s=0;
+ }
+ Timecalc(int h,int m,int s)
+ {
+  this.h=h;
+  this.m=m;
+  this.s=s;
+ }
+ void advance(int a,int b,int c)
+ {
+  h+=a;
+  m+=b;
+  s+=c;
+  while(s>=60)
+  {
+   s-=60;
+   m+=1;
+  }
+  while(m>=60)
+  {
+   m-=60;
+   h+=1;
+  }
+  while(h>=24)
+  {
+   h-=24;
+  }
+ }
+ void output()
+ {
+  System.out.println("The time is "+h+":"+m+":"+s);
+ } 
+ void reset(int a,int b,int c)
+ {
+  h=a;
+  m=b;
+  s=c;
+ }
+}
+class Time
+{
+ public static void main(String args[])
+ {
+  Timecalc t=new Timecalc();
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the current time");
+  t.h=sc.nextInt();
+  t.m=sc.nextInt();
+  t.s=sc.nextInt();
+  int sel,op;
+  do
+  {
+   System.out.println("Choose an Option to perform:");
+   System.out.println("1.Advance");    
+   System.out.println("2.Output"); 
+   System.out.println("3.Reset"); 
+   op=sc.nextInt();
+   
+   switch(op)
+   {
+    case 1:{
+    				System.out.println("Enter the time to advance to"); 
+    				int a=sc.nextInt();
+    				int b=sc.nextInt();
+    				int c=sc.nextInt();
+    				t.advance(a,b,c);
+    				break;
+    			 }
+    case 2:{
+    				t.output();
+    				break;
+    			 }
+    case 3:{
+    				System.out.println("Enter new time");
+    				int x=sc.nextInt();
+    				int y=sc.nextInt();
+    				int z=sc.nextInt();
+    				t.reset(x,y,z);
+    				break;
+    			 }
+    default:System.out.println("Invalid option!");
+   }
+   System.out.println("Do you wish to continue?(press 1)");
+   sel=sc.nextInt();
+  }while(sel==1);
+ }
+} 
+    					
+    
+    				             
